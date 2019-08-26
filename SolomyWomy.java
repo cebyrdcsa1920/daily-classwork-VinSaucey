0686ba200f608631ed1b541d18fb1877e85ad0da
VinSaucey <vincentlawsauseda@gmail.com>
2019-08-26 14:12:05 +0000

    Make week 3 folder and student class

--- SolomyWomy.java
+++ SolomyWomy.java
@@ -3,7 +3,11 @@
  * a comment in java.
  */
 
- //This is how you do it on one line
+ //This is how you do it on one line//
+
+ /*This is another way to comment
+ on more than one line.*/
+
 
 
 import java.awt.Graphics;
