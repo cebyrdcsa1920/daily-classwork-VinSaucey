120c59a8489254b0d4e3fa0caf6ecc10e9854d30
VinSaucey <vincentlawsauseda@gmail.com>
2019-09-24 14:14:40 +0000

    add to my tempConverter class

--- week7/TempConverter.java
+++ week7/TempConverter.java
@@ -14,6 +14,16 @@ public class TempConverter
         if (isCelsius)
         {
             temperature = temperature * 9 / 5 + 32;
+            isCelsius = false;
+        }
+    }
+
+    public void convertToC()
+    {
+        if (!isCelsius)
+        {
+            temperature = (temperature - 32) * 5 / 9;
+            isCelsius = true;
         }
     }
 
@@ -29,5 +39,16 @@ public class TempConverter
             result += " Fahrenheit";
         }
         return result;
+    }
+
+    public static void main(String[] args)
+    {
+        TempConverter myTemp = new TempConverter(25.6);
+        System.out.println(myTemp);
+        myTemp.convertToF();
+        System.out.println(myTemp);
+        myTemp.convertToC();
+        System.out.println(myTemp);
+
     }
 }
